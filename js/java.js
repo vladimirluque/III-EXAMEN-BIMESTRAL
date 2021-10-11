bf2fd561c5c7b95393aa2561caa6ee667fe745a5
vladimir <vladimirluquellanqui@gmail.com>
2021-10-11 12:22:22 -0500

    first commit

---
+++ js/java.js
@@ -0,0 +1,52 @@
+let cancion = document.getElementById("cancion");
+let reproducir = document.getElementById("reproducir");
+reproducir.addEventListener("click", () =>{
+  cancion.play();
+})
+let cancion1 = document.getElementById("cancion1");
+let reproducir1 = document.getElementById("reproducir1");
+reproducir1.addEventListener("click", () =>{
+  cancion1.play();
+})
+let cancion2 = document.getElementById("cancion2");
+let reproducir2 = document.getElementById("reproducir2");
+reproducir2.addEventListener("click", () =>{
+  cancion2.play();
+})
+let cancion3 = document.getElementById("cancion3");
+let reproducir3 = document.getElementById("reproducir3");
+reproducir3.addEventListener("click", () =>{
+  cancion3.play();
+})
+let cancion4 = document.getElementById("cancion4");
+let reproducir4 = document.getElementById("reproducir4");
+reproducir4.addEventListener("click", () =>{
+  cancion4.play();
+})
+
+let cancion5 = document.getElementById("cancion5");
+let reproducir5 = document.getElementById("reproducir5");
+reproducir5.addEventListener("click", () =>{
+  cancion5.play();
+})
+
+let cancion6 = document.getElementById("cancion6");
+let reproducir6 = document.getElementById("reproducir6");
+reproducir6.addEventListener("click", () =>{
+  cancion6.play();
+})
+let cancion7 = document.getElementById("cancion7");
+let reproducir7 = document.getElementById("reproducir7");
+reproducir7.addEventListener("click", () =>{
+  cancion7.play();
+})
+
+let cancion8 = document.getElementById("cancion8");
+let reproducir8 = document.getElementById("reproducir8");
+reproducir8.addEventListener("click", () =>{
+  cancion8.play();
+})
+
+
+
+
